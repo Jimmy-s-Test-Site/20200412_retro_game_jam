61b71e061c76b70ac97d10a8b0a2b313b19bc058
Fenguinn <salem.abdulla15@gmail.com>
2020-12-04 23:22:05 -0500

    here you go sean

--- Abdullah/Player.gd
+++ Abdullah/Player.gd
@@ -7,34 +7,49 @@ export (int) var gravity = 1200
 var velocity = Vector2()
 var midAir = false
 
+var jumpInput 
+var headInput
+var shake
+var stomp
+
 func get_input():
 	velocity.x = 0
-	var jumpInput = Input.is_action_just_pressed("jump")
-	var headInput = Input.is_action_just_pressed("action1")
-	var shake = Input.is_action_just_pressed("action2")
-	var stomp = Input.is_action_just_pressed("action3")
+
+	jumpInput = Input.is_action_just_pressed("jump")
+	headInput = Input.is_action_just_pressed("action1")
+	shake = Input.is_action_just_pressed("action2")
+	stomp = Input.is_action_just_pressed("action3")
 
 	if jumpInput and is_on_floor():
-		$AnimationPlayer.play("Jump")
 		midAir = true
 		velocity.y = jump_speed
-	
-	if headInput:
-		$AnimationPlayer.play("Headbang")
-	elif shake:
-		$AnimationPlayer.play("Tailshake")
-	elif stomp:
-		$AnimationPlayer.play("Stomp")
 
 func _physics_process(delta):
 	get_input()
+
 	velocity.y += gravity * delta
+
 	if midAir and is_on_floor():
 		midAir = false
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+	animation_manager()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func animation_manager():
+	if is_on_floor():
+		if headInput:
+			$AnimationPlayer.play("Headbang")
+		elif shake:
+			$AnimationPlayer.play("Tailshake")
+		elif stomp:
+			$AnimationPlayer.play("Stomp")
+		else:
+			$AnimationPlayer.play("Walking")
+	else:
+		$AnimationPlayer.play("Jump")
 
