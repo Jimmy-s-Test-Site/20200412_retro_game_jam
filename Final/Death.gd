1cc63ca5592665035b1061cfe1dc8d23e90c6743
Julian <jaacko.torus@gmail.com>
2020-12-06 23:36:54 -0500

    Working death scene

--- Final/Death.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-var count : float = 0
-
-func _process(delta):
-	self.count = (self.count delta) % 256
-	$ColorRect.color = Color.from_hsv()
--- Final/Game.gd
+++ Final/Game.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
+export (Resource) var raw_death_scene
+
+onready var death_scene
+
 var rng = RandomNumberGenerator.new()
 
 export (Array, Resource) var raw_levels
+
+var curr_level
 
 var levels = []
 
@@ -13,12 +19,12 @@ func load_levels():
 func instance_rand_level():
 	var random_index = rng.randi() % self.levels.size()
 	var random_level = self.levels[random_index]
-	var scene_instance = random_level.instance()
+	self.curr_level = random_level.instance()
 	
-	scene_instance.get_node("Level").connect("player_died", self, "_on_player_died")
-	scene_instance.get_node("Level").connect("player_earned_points", self, "_on_player_earned_points")
-	scene_instance.z_index = -1
-	add_child(scene_instance)
+	self.curr_level.get_node("Level").connect("player_died", self, "_on_player_died")
+	self.curr_level.get_node("Level").connect("player_earned_points", self, "_on_player_earned_points")
+	self.curr_level.z_index = -1
+	add_child(self.curr_level)
 	
 	$ScoreContainer.scale *= get_viewport().size / Vector2(
 		ProjectSettings.get("display/window/size/width"),
@@ -30,9 +36,19 @@ func _ready():
 	rng.randomize()
 	self.load_levels()
 	self.instance_rand_level()
+	
+	self.death_scene = load(self.raw_death_scene.resource_path)
 
 func _on_player_died():
-	print("oops")
+	self.curr_level.queue_free()
+	
+	var death_scene_instance = self.death_scene.instance()
+	add_child(death_scene_instance)
+	
+	death_scene_instance.scale *= get_viewport().size / Vector2(
+		ProjectSettings.get("display/window/size/width"),
+		ProjectSettings.get("display/window/size/height")
+	)
 
 func _on_player_earned_points(amount : float) -> void:
 	print("amount earned")
